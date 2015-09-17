b77703552bf2164a235eeac16d136e75f8d05ec1
Chase Conley <chaseeconley@gmail.com>
2015-09-17 10:51:45 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,57 @@
+function stroke(direction, pause, data){
+  this.direction = direction;
+  this.pause = pause;
+  this.data = [];
+  this.pairs = data.split(";");
+  for (i = 0; i < this.pairs.length; i++){
+    this.data.push(this.pairs[i].split(","));
+  }
+  
+  this.maxX = function (){
+    var max = 0;
+    for (i = 0; i < this.data.length; i++){
+      if (parseInt(this.data[i][0]) > max) max = parseInt(this.data[i][0]);
+    }
+    return max;
+  }
+  
+  this.maxY = function (){
+    var max = 0;
+    for (i = 0; i < this.data.length; i++){
+      console.log(i)
+      if (parseInt(this.data[i][1]) > max) max = parseInt(this.data[i][1]);
+    }
+    return max;
+  }
+  
+  this.minX = function (){
+    var min = 100000000;
+    for (i = 0; i < this.data.length; i++){
+      if (parseInt(this.data[i][0]) < min) min = parseInt(this.data[i][0]);
+    }
+    return min;
+  }
+  
+  this.minY = function (){
+    var min = 100000000;
+    for (i = 0; i < this.data.length; i++){
+      if (parseInt(this.data[i][1]) < min) min = parseInt(this.data[i][1]);
+    }
+    return min;
+  }
+  
+  this.polygonPoints = function (){
+    var string = "";
+    for (i = 0; i < this.data.length; i++){
+      string = string + this.data[i][0] + "," + this.data[i][1] + " ";
+    }
+    return string;
+  }
+
+}
+
+var s = new stroke(1, 0, "151,87;167,87;167,245;166,249;163,253;160,255;151,240;151,87");
+
+function showAlert(){
+  alert(s.direction);
+}
